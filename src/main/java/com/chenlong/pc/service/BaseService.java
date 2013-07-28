775292075898fda7ec777c180de09cd5354dd324
chen6516@qq.com <chen6516@qq.com>
2013-07-28 19:26:11 +0800

    new commit

--- src/main/java/com/chenlong/pc/service/BaseService.java
+++ src/main/java/com/chenlong/pc/service/BaseService.java
@@ -1,6 +1,7 @@
 package com.chenlong.pc.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.chenlong.pc.entity.Entity;
 import com.chenlong.pc.entity.Log;
@@ -20,17 +21,17 @@ public interface BaseService<T extends Entity> {
 	
 	public void update(T obj, Log log);*/
 	
-	public List<T> findAll();
+	public List<T> findAll(Map<String, Object> params);
 	
-	public T findById(String id);
+	public T findById(Map<String, Object> params);
 	
-	public List<T> findTop(int from, int limit, String homeId);
+	public List<T> findTop(Map<String, Object> params);
 	
-	public int countAll(String homeId);
+	public int countAll(Map<String, Object> params);
 	
-	public void delete(String id, Log log);
+	public void delete(Map<String, Object> params);
 	
-	public void save(T obj, Log log);
+	public void save(Map<String, Object> params);
 	
-	public void update(T obj, Log log);
+	public void update(Map<String, Object> params);
 }
